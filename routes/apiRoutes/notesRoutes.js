a3dff8b51f3a03eb457a44d92323a3c6204f12e5
aldrinburgos <burgos.aldrin@gmail.com>
2020-12-12 15:58:38 -0600

    add post functionality

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,12 +1,27 @@
 const router = require('express').Router();
+const fs = require('fs');
 
 const notes = require('../../db/db.json');
+const { createNote } = require ('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     let results = notes;
 
     res.json(results);
     console.log(results)
+    /* fs.readFile('./db/db.json', (err, data) => {
+        if(err) throw(err);
+        res.json(JSON.parse(data));
+    }); */
+});
+
+router.post('/notes', (req, res) => {
+    //add id to note (add 1 to last value)
+    req.body.id = notes.length.toString();
+
+    const note = createNote(req.body, notes);
+    
+    res.json(note);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+//parse data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//serve static files
 app.use(express.static('public'));
 
+//routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+//listener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
