76415af9245aa72436c5456a6ca9b6e16ae64b76
aldrinburgos <burgos.aldrin@gmail.com>
2020-12-12 17:03:01 -0600

    add delete function (deletes in array but doesn't reflect page)

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,21 +1,30 @@
 const router = require('express').Router();
 
 const notes = require('../../db/db.json');
-const { createNote } = require ('../../lib/notes');
+const { createNote, deleteNote } = require ('../../lib/notes');
 
+//get notes from json
 router.get('/notes', (req, res) => {
     let results = notes;
 
     res.json(results);
-    console.log(results)
 });
 
+//add notes to json
 router.post('/notes', (req, res) => {
     //add id to note (add 1 to last value)
     req.body.id = notes.length.toString();
 
     const note = createNote(req.body, notes);
     
+    res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+
+    const note = deleteNote(id);
+
     res.json(note);
 });
 
