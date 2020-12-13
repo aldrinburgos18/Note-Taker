84c856f3e67c54e6de1ee9ff5f897e254d887282
aldrinburgos <burgos.aldrin@gmail.com>
2020-12-12 21:28:18 -0600

    finalize

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,31 +1,34 @@
 const router = require('express').Router();
+const fs = require('fs');
 
-const notes = require('../../db/db.json');
 const { createNote, deleteNote } = require ('../../lib/notes');
 
 //get notes from json
 router.get('/notes', (req, res) => {
-    let results = notes;
-
-    res.json(results);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const obj = JSON.parse(data);
+        res.json(obj)
+        }) 
 });
 
 //add notes to json
 router.post('/notes', (req, res) => {
-    //add id to note (add 1 to last value)
-    req.body.id = notes.length.toString();
-
-    const note = createNote(req.body, notes);
-    
-    res.json(note);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const obj = JSON.parse(data);
+        //add id to note (add 1 to last value)
+        req.body.id = obj.length.toString();
+        res.json(createNote(req.body, obj));
+        });
 });
 
 router.delete('/notes/:id', (req, res) => {
     const id = req.params.id;
 
-    const note = deleteNote(id);
-
-    res.json(note);
+    deleteNote(id).then(data => {
+        res.json(data);
+    });
 });
 
 module.exports = router;
